dcbe3d8330fbfbd54914b6b033c1167626dc3079
kesava-infos <67364912+kesava-infos@users.noreply.github.com>
2022-01-13 16:23:15 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Scroll />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Scroll from "./components/scrollExample"
+export default {
+  name: "App",
+  components: {
+    Scroll
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/scrollExample.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="head">
+    <div class="head">
+    <button @click="goto('div1')">Div 1</button>
+    <button @click="goto('div2')">Div 2</button>
+    <button @click="goto('div3')">Div 3</button>
+    </div>
+    <br/>
+
+    <div class="div" ref="div1">This is Div 1</div>
+    <div class="div" ref="div2">This is Div 2</div>
+    <div class="div" ref="div3">This is Div 3</div>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+    goto(refName) {
+      var element = this.$refs[refName];
+      var top = element.offsetTop;
+
+      window.scrollTo(0, top);
+    }
+  }
+  }
+</script>
+<style>
+.div {
+  height: 1000px;
+  padding: 20px;
+  border: 2px solid yellow;
+}
+.head{
+  position: fixed;
+  bottom:0px
+}
+</style>
